f8feda6636c264b9edc2db08a85b6494a967e645
Youcef Bellouche <fecuoy03@gmail.com>
2022-09-12 15:33:50 +0100

    facebook multi pixel

---
+++ cod-fb-multi-pixel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Facebook Multi pixel COD
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Youcef Bellouche
+ * Author URI:        https://www.facebook.com/bellou.fecuoy2000/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+// Define Constants
+define( 'COD_FB_MP_PLUGIN_VERSION', '1.0.0' );
+define( 'COD_FB_MP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'COD_FB_MP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'COD_FB_MP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
